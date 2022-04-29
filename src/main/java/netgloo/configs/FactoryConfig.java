2bc9cc1c4bb331bcf0906d62e89e89847272fe3a
subhashreerout1 <101645362+subhashreerout1@users.noreply.github.com>
2022-04-29 17:57:23 +0530

    mamuni

--- src/main/java/netgloo/configs/FactoryConfig.java
+++ src/main/java/netgloo/configs/FactoryConfig.java
@@ -8,7 +8,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 @EnableAutoConfiguration
 public class FactoryConfig {
 
-//main2 gulu
+//main2 mamuni
 	@Bean
 	public HibernateJpaSessionFactoryBean sessionFactory() {
 	    return new HibernateJpaSessionFactoryBean();
